28c295aaf807bcd3ff8a25bebda6c43666e5004e
lars <larsvk@gmail.com>
2016-10-25 16:01:01 +0200

    nav step one works

--- app/src/main/java/lars/be/movieapp/MovieDetails.java
+++ app/src/main/java/lars/be/movieapp/MovieDetails.java
@@ -3,10 +3,21 @@ package lars.be.movieapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.omertron.themoviedbapi.model.movie.MovieInfo;
 
+import java.net.URL;
+
+import static lars.be.movieapp.R.id.imageView;
+
 public class MovieDetails extends AppCompatActivity {
+
+    private ImageView imageViewDetails;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +27,12 @@ public class MovieDetails extends AppCompatActivity {
         Intent i = getIntent();
         MovieInfo movieInfo = (MovieInfo) i.getExtras().get("movieInfo");
 
+        imageViewDetails = (ImageView) findViewById(R.id.imageViewDetails);
 
+        URL imageUrl = null;
+
+        imageUrl = AppController.getInstance().createImageUrl(movieInfo.getBackdropPath(), "w500");
+        ImageLoader.getInstance().displayImage(imageUrl.toString(), imageViewDetails);
 
 
     }
